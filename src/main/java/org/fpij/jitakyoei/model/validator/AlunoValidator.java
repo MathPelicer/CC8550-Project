cec8933aa145703c0a99c8349f16331db4786b3e
MathPelicer <matheuspelicer58@gmail.com>
2022-11-06 14:20:24 -0300

    [remove] removed icon images [code] AlunoValidator.java implementation started [code] AlunosBOImpl.java modified for validator implementation

--- src/main/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
+++ src/main/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
@@ -1,9 +1,14 @@
 package org.fpij.jitakyoei.model.validator;
 
 import org.fpij.jitakyoei.model.beans.Aluno;
+import org.fpij.jitakyoei.model.beans.Filiado;
 
 public class AlunoValidator implements Validator<Aluno> {
 	public boolean validate(Aluno obj) {
+		Filiado filiado = obj.getFiliado();
+		if (filiado.getCpf() == null || filiado.getCpf() == "") {
+			return false;
+		}
 		return true;
 	}
 }
